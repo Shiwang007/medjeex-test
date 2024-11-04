c79ea339c11d91e5a1942fbb562a4a3ce0b5d478
Shiwang007 <shiwangkush007@gmail.com>
2024-11-04 13:32:13 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -78,7 +78,6 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.matchPassword = async function (password) {
-  console.log(password + "  " + this.password);
   return await bcrypt.compare(password, this.password);
 };
 
